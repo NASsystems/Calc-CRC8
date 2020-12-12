567577b21b9c7133d61a2ec905389ee3eaa31889
Isao Sato <isaosato@outlook.jp>
2020-12-12 16:35:55 +0900

    Update Cacl-CRC8Lib.ps1

--- Cacl-CRC8Lib.ps1
+++ Cacl-CRC8Lib.ps1
@@ -16,6 +16,31 @@ $linedigit = '0'*([Math]::Max(2, [Math]::Floor([Math]::Log10($crclist.Count))+1)
 $formatstring = "{0:$linedigit} : 0x{1:x2}"
 $linecount = 1
 $crclist |% {$formatstring -f $linecount, $_; ++$linecount}
+.Note
+Calc-CRC8 version 1.00
+
+MIT License
+
+Copyright (c) 2020 Isao Sato
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
     Begin {
         $table = New-Object Byte[] 256
